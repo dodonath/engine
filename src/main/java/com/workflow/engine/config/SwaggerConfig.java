a8d5cfb0fb7d4bf6fe81a9ae1eb07dc6b4f4d8f5
dodonath <30523350+dodonath@users.noreply.github.com>
2019-01-28 12:34:13 +0530

    Minor changes

--- src/main/java/com/workflow/engine/config/SwaggerConfig.java
+++ src/main/java/com/workflow/engine/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.syra.workflow.engine.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.workflow.engine.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
